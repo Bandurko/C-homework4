b9d10dbe5f3d3c28079fb6503baff001a66ad7e9
“Sergey” <s22-01-15@yandex.ru>
2023-02-08 06:55:59 +1000

    task29

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,3 +5,23 @@
 // 9012 -> 12
 
 // Важно: не использовать строки, использовать только арифметические действия.
+
+int GetNumbersSum(int num){ // n=3
+
+    int count = 0; // count 0
+    if (num < 0)
+        num *= (-1); // n = n * (-1) 
+    while (n > 0) // 0 > 0
+    {
+        n /= 10; // n = n / 10    n = 0
+        count++; // count 3
+        
+    }
+    return count;
+
+    int result = 1;
+    for (int i = 2; i <= count; i++){ // i <= n; 2 <= 3
+        result *= i; // result
+    }
+    return result;
+}
---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GetArray(int num){
+    int[] result = new int[8];
+    for (int i = 0; i < 8; i++){
+        result[i] = new Random().Next((num + 1));
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите максимальное число в массиве: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = GetArray(n);
+Console.Write($"[{String.Join(", ", array)}]");
