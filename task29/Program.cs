6cb0e3b62cf7ce7fc3ff53597734d428738ce0de
“Sergey” <s22-01-15@yandex.ru>
2023-02-09 05:15:38 +1000

    task29

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,15 +1,17 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int[] GetArray(int num){
+int[] GetArray(int min, int max){
     int[] result = new int[8];
     for (int i = 0; i < 8; i++){
-        result[i] = new Random().Next((num + 1));
+        result[i] = new Random().Next(min, (max + 1));
     }
     return result;
 }
 
 Console.Clear();
+Console.Write("Введите минимальное число, которое может быть в массиве: ");
+int n1 = int.Parse(Console.ReadLine());
 Console.Write("Введите максимальное число, которое может быть в массиве: ");
-int max = int.Parse(Console.ReadLine());
-int[] array = GetArray(max);
+int n2 = int.Parse(Console.ReadLine());
+int[] array = GetArray(n1, n2);
 Console.Write($"[{String.Join(", ", array)}]");
