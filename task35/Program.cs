63808b5f6e1cf743f9075940f409d817424edc7a
“Sergey” <s22-01-15@yandex.ru>
2023-02-13 08:35:54 +1000

    add coments in task35

--- task35/Program.cs
+++ task35/Program.cs
@@ -2,10 +2,6 @@
 // Задача 35:Задайте одномерный массив из 123 случайных чисел [0: 1000].
 // Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 
-// Доп
-// Задача 35:Задайте одномерный массив из 123 случайных чисел [0: 1000].
-// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
-
 // Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
 
 // [5, 18, 123, 6, 2] -> 1
@@ -14,17 +10,9 @@
 
 // [10, 11, 12, 13, 14]-> 5
 
-int[] array = GetRandomArray(123, 0, 1000);
+int[] array = GetRandomArray(123, 0, 1000); // Задает массив с заданными параметрами
 
-int indexSum = 0;
-foreach (int index in array)
-{
-    indexSum += index >= 10 && index < 100 ? 1 : 0;
-}
-
-Console.WriteLine($"[{String.Join(" ",array)}] -> {indexSum}");
-
-int[] GetRandomArray(int size, int minValue, int maxValue)
+int[] GetRandomArray(int size, int minValue, int maxValue) // Метод (функция) для генерации элементов массива по заданным параметрам
 {
     int[] result = new int[size];
     for (int i = 0; i < size; i++)
@@ -34,3 +22,18 @@ int[] GetRandomArray(int size, int minValue, int maxValue)
 
     return result;
 }
+
+int indexSum = FindIndexSumSegment(10, 100); // Задает отрезок массив с заданными параметрами
+
+int FindIndexSumSegment(int startSegment, int endSegment) // Метод (функция), который находит и суммирует элементы массива на заданном отрезке
+{
+    int indexSum = 0;
+    foreach (int index in array)
+    {
+        indexSum += index >= startSegment && index < endSegment ? 1 : 0;
+    }
+
+    return indexSum;
+}
+
+Console.WriteLine($"В массиве [{String.Join(" ",array)}], на отрезке от 10 до 99 находятся {indexSum} элементов."); // Выводит полученные данные в консоль
